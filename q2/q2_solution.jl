8d46c2b78724b6cd8cbbc37cccfa9ba0339d908b
Dan Z <dan.zaikin@gmail.com>
2021-08-25 09:20:58 +1000

    solution w/ output

--- q2/q2_solution.jl
+++ q2/q2_solution.jl
@@ -1,33 +1,33 @@
 using HTTP, JSON, DataFrames, CSV
 
-r = HTTP.request("GET", "https://raw.githubusercontent.com/yoninazarathy/ProgrammingCourse-with-Julia-SimulationAnalysisAndLearningSystems/main/practicals_jupyter/practical_B_julia_essentials.ipynb")
-test_json = JSON.parse(String(r.body))
 
-println(r.status)
+# """
+# println(r.status)
 
-#println(test_json)
+# #println(test_json)
 
-println(length(test_json["cells"]))
-code_cells_count = count(cell->(cell["cell_type"] == "code"), test_json["cells"])
-println(code_cells_count)
-println(count(cell->(cell["cell_type"] == "markdown"), test_json["cells"]))
-code_cells_nooutput_count = count(cell->(cell["cell_type"] == "code" && length(cell["outputs"]) == 0), test_json["cells"])
-println(code_cells_nooutput_count)
-println(code_cells_count - code_cells_nooutput_count)
-println(String(test_json["cells"][1]["source"][1]))
+# println(length(test_json["cells"]))
+# code_cells_count = count(cell->(cell["cell_type"] == "code"), test_json["cells"])
+# println(code_cells_count)
+# println(count(cell->(cell["cell_type"] == "markdown"), test_json["cells"]))
+# code_cells_nooutput_count = count(cell->(cell["cell_type"] == "code" && length(cell["outputs"]) == 0), test_json["cells"])
+# println(code_cells_nooutput_count)
+# println(code_cells_count - code_cells_nooutput_count)
+# println(String(test_json["cells"][1]["source"][1]))
+# """
+# """
+# struct SharedCellCounts
+#     character_count::Int
+#     line_count::Int
+# end
 
-struct SharedCellCounts
-    character_count::Int
-    line_count::Int
-end
-
-struct MarkDownCellCounts
-    one_hash_count::Int
-    two_hash_count::Int
-    three_hash_count::Int
-    four_hash_count::Int
-end
-
+# struct MarkDownCellCounts
+#     one_hash_count::Int
+#     two_hash_count::Int
+#     three_hash_count::Int
+#     four_hash_count::Int
+# end
+# """
 function get_cell_counts_shared(cell)
     char_total, lines_total = 0, 0
     for line in cell["source"]
@@ -82,13 +82,15 @@ function get_cell_counts_code(cell)
     return count
 end
 
-function process_cells_markdown(markdown_cells)
-    df = DataFrame(character_count = Int[], line_count = Int[], one_hash = Int[], two_hash = Int[], three_hash = Int[], four_hash = Int[])
-    for cell in markdown_cells
-        push!(df, [get_cell_counts_shared(cell)..., get_cell_counts_markdown(cell)...])
-    end
-    return df
-end
+# """
+# function process_cells_markdown(markdown_cells)
+#     df = DataFrame(character_count = Int[], line_count = Int[], one_hash = Int[], two_hash = Int[], three_hash = Int[], four_hash = Int[])
+#     for cell in markdown_cells
+#         push!(df, [get_cell_counts_shared(cell)..., get_cell_counts_markdown(cell)...])
+#     end
+#     return df
+# end
+# """
 
 function process_cell_markdown(cell_number, markdown_cell)
     return [cell_number, get_cell_counts_shared(markdown_cell)..., get_cell_counts_markdown(markdown_cell)...]
@@ -111,39 +113,57 @@ function process_cells_all(all_cells)
     end
     return markdown_df, code_df
 end
+# """
+# function process_cells_code(code_cells)
+#     df = DataFrame(character_count = Int[], line_count = Int[], returns = Int[], fors = Int[], ifs = Int[], usings = Int[])
+#     for cell in code_cells
+#         push!(df, [get_cell_counts_shared(cell)..., get_cell_counts_code(cell)...])
+#     end
+#     return df
+# end
+# """
+r = HTTP.request("GET", "https://raw.githubusercontent.com/yoninazarathy/ProgrammingCourse-with-Julia-SimulationAnalysisAndLearningSystems/main/practicals_jupyter/practical_B_julia_essentials.ipynb")
 
-function process_cells_code(code_cells)
-    df = DataFrame(character_count = Int[], line_count = Int[], returns = Int[], fors = Int[], ifs = Int[], usings = Int[])
-    for cell in code_cells
-        push!(df, [get_cell_counts_shared(cell)..., get_cell_counts_code(cell)...])
+test_json = JSON.parse(String(r.body))
+# total_number
+# code_number
+# markdown_number
+# code_nooutput_number
+# code_output_number
+# output_keys_number 
+
+
+println("Total number of cells: " * string(length(test_json["cells"])))
+code_cells_count = count(cell->(cell["cell_type"] == "code"), test_json["cells"])
+println("Number of code cells: " * string(code_cells_count))
+println("Number of markdown cells: " * string(count(cell->(cell["cell_type"] == "markdown"), test_json["cells"])))
+code_cells_nooutput_count = count(cell->(cell["cell_type"] == "code" && length(cell["outputs"]) == 0), test_json["cells"])
+println("Number of code cells w/o output: " * string(code_cells_nooutput_count))
+println("Number of code cells with output: " * string(code_cells_count - code_cells_nooutput_count))
+
+function total_keys_in_outputs(cells)
+    unique_keys = Set()
+    for cell in cells
+        if haskey(cell, "outputs")
+            for output in cell["outputs"]
+                union!(unique_keys, Set(keys(output)))
+                for key in keys(output)
+                    if isa(output[key], Dict)
+                        union!(unique_keys, Set(keys(output[key])))
+                    end
+                end
+            end
+        end
     end
-    return df
+    return unique_keys
 end
+
+
+# It always worried me whether I missed an edge case in the outputs
+println("Number of unique keys present in output fields: " * string(length(total_keys_in_outputs(test_json["cells"]))))
 
 markdown_df, code_df = process_cells_all(test_json["cells"])
 
-CSV.write("testfile.csv", markdown_df; header = ["cell_number", "character_count", "line_count", "#", "##", "###", "####"])
+CSV.write("markdown_summary.csv", markdown_df; header = ["cell_number", "character_count", "line_count", "#", "##", "###", "####"])
 
-CSV.write("testfile2.csv", code_df; header = ["cell_number", "character_count", "line_count", "return", "for", "if", "using"])
-
-"""
-source = test_json["cells"][20]
-
-@show get_cell_counts_shared(source)
-@show get_cell_counts_markdown(source)
-@show get_cell_counts_markdown(source)
-@show get_cell_counts_code(test_json["cells"][8])
-
-df = DataFrame(A = Int[], B = Int[], C = Int[], D = Int[])
-
-push!(df, get_cell_counts_markdown(source))
-push!(df, get_cell_counts_code(test_json["cells"][8]))
-
-println(df)
-
-@show markdown_processed = process_cells_markdown(filter(cell -> (cell["cell_type"] == "markdown"), test_json["cells"]))
-
-DataFrames.insertcols!(markdown_processed, 1, :cell_number => 1:nrow(markdown_processed))
-
-CSV.write("testfile.csv", markdown_processed; header = ["cell_number", "character_count", "line_count", "#", "##", "###", "####"])
-"""
+CSV.write("code_summary.csv", code_df; header = ["cell_number", "character_count", "line_count", "return", "for", "if", "using"])
